df340a5995a4359404e21fcb278d0f818e8023cc
Aanuj55 <44504592+Aanuj55@users.noreply.github.com>
2018-11-26 19:28:18 +0530

    Counter
    count up and down on click

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./styles.css";
+import Counter from "./counterApp";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Hiration Interns</h1>
-      <h2>Lets iracle!</h2>
+      <h2>Lets create some miracle!</h2>
+      <Counter />
     </div>
   );
 }
